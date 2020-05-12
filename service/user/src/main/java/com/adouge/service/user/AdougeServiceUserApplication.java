dfd271b6817b385f39f42609e012631ec96e8ea7
vinson <646911949@qq.com>
2020-05-12 20:44:35 +0800

    初始化项目框架2

--- service/user/src/main/java/com/adouge/service/user/AdougeServiceUserApplication.java
+++ service/user/src/main/java/com/adouge/service/user/AdougeServiceUserApplication.java
@@ -1,4 +1,4 @@
-package com.adouge.auth;
+package com.adouge.service.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class AdougeAuthApplication {
+public class AdougeServiceUserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdougeAuthApplication.class, args);
+        SpringApplication.run(AdougeServiceUserApplication.class, args);
     }
 
 }
