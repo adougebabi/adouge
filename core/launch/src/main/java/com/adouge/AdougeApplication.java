fcad2be71ddc054cc465b0ef0754440f7a7d2010
vinson <646911949@qq.com>
2020-05-18 13:13:25 +0800

    修复pom无法编译

--- core/launch/src/main/java/com/adouge/AdougeApplication.java
+++ core/launch/src/main/java/com/adouge/AdougeApplication.java
@@ -1,17 +1,8 @@
 package com.adouge;
 
-import com.adouge.core.launch.constant.AppConstant;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.*;
 import org.springframework.util.Assert;
-import org.springframework.util.StringUtils;
-
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
 /**
  * @author : Vinson
  * @date : 2020/5/16 4:17 下午
--- service/user/src/main/java/com/adouge/service/user/AdougeServiceUserApplication.java
+++ service/user/src/main/java/com/adouge/service/user/AdougeServiceUserApplication.java
@@ -2,13 +2,9 @@ package com.adouge.service.user;
 
 import com.adouge.AdougeApplication;
 import com.adouge.core.launch.constant.AppConstant;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
 //@SpringCloudApplication
@@ -18,7 +14,7 @@ public class AdougeServiceUserApplication {
 
     public static void main(String[] args) {
 //        SpringApplication.run(AdougeServiceUserApplication.class, args);
-        AdougeApplication.run("adouge-service-user",AdougeServiceUserApplication.class,args);
+        AdougeApplication.run("adouge-service-user", AdougeServiceUserApplication.class, args);
     }
 
 }
